6bf6bf09fc6bb14caa82372bcb1f5683bee4b759
jonas.schrage <119843859+joschrag@users.noreply.github.com>
2024-06-16 21:37:17 +0200

    [#5]: Initial commit

---
+++ src/app/pages/game.py
@@ -0,0 +1,11 @@
+import dash
+
+dash.register_page(
+    __name__,
+    path="/",
+    name="Blackjack Trainer",
+    title="Blackjack Trainer",
+    description="Landing page.",
+)
+
+layout: list = []
---
+++ src/app/pages/settings.py
@@ -0,0 +1,10 @@
+import dash
+
+dash.register_page(
+    __name__,
+    path="/settings",
+    name="Blackjack Trainer Settings",
+    title="Blackjack Trainer Settings",
+    description="Settings page.",
+)
+layout: list = []
