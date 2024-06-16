e79778fd797700094702596b5b69b169b20fa769
jonas.schrage <119843859+joschrag@users.noreply.github.com>
2024-06-16 21:58:27 +0200

    [#7]: Add initial sqlite file creation

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,9 @@
+import pathlib
+
 import sqlalchemy as sa
 
+db_file = pathlib.Path.cwd() / "db" / "db.sqlite"
+if not db_file.exists():
+    with open(db_file, "w") as fp:
+        pass
 engine = sa.create_engine("sqlite:///db/db.sqlite")
