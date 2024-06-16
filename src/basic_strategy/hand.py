82d0c0208ea0e545a1b840d4da3fad93333d7670
jonas.schrage <119843859+joschrag@users.noreply.github.com>
2024-06-16 23:12:57 +0200

    [#7]: Add Unicode representation to Card class

--- src/basic_strategy/hand.py
+++ src/basic_strategy/hand.py
@@ -14,16 +14,40 @@ CARD_VALS: dict = {
     "A": 11,
 }
 
+SUIT_UNICODE: dict = {
+    "s": 0x1F0A0,
+    "h": 0x1F0B0,
+    "d": 0x1F0C0,
+    "c": 0x1F0D0,
+}
+RANK_UNICODE: dict = {
+    "A": 0x1,
+    "2": 0x2,
+    "3": 0x3,
+    "4": 0x4,
+    "5": 0x5,
+    "6": 0x6,
+    "7": 0x7,
+    "8": 0x8,
+    "9": 0x9,
+    "T": 0xA,
+    "J": 0xB,
+    "Q": 0xD,
+    "K": 0xE,
+}
+
 
 class Card:
     suit: str
     rank: str
     value: int
+    unicode: str
 
     def __init__(self, suit, rank) -> None:
         self.suit = suit
         self.rank = rank
         self.value = CARD_VALS[rank]
+        self.unicode = chr(SUIT_UNICODE[suit] + RANK_UNICODE[rank])
 
     def __str__(self) -> str:
         return f"<{self.rank}{self.suit}>"
