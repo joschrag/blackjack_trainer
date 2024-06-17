1702e618dafb740a287c7b6afb17002463a98fd8
jonas.schrage <119843859+joschrag@users.noreply.github.com>
2024-06-17 21:38:28 +0200

    [#7]: Add blackjacktable and buttons for blackjack actions

--- src/app/pages/game.py
+++ src/app/pages/game.py
@@ -1,5 +1,8 @@
 import dash
-from dash import html
+import dash_bootstrap_components as dbc
+from dash import dcc, html
+
+from src.app import game_callbacks  # noqa: F401
 
 dash.register_page(
     __name__,
@@ -9,5 +12,44 @@ dash.register_page(
     description="Landing page.",
 )
 
+gamemode_dd = dcc.Dropdown(
+    ["basic", "hard", "soft", "split"],
+    "basic",
+    clearable=False,
+    id="gamemode_dd",
+    persistence=True,
+    persistence_type="session",
+)
+user_input = dcc.Input(
+    type="text", placeholder="Enter your name", id="user_input", persistence=True, persistence_type="session"
+)
+start_button = html.Button("Deal Cards", id="start_btn", className="gamebtn")
+correct_choice = html.Div(id="correct_choice")
 
-layout: list = [html.Div([], id="bj-table")]
+double_button = html.Button("double or hit (d)", id="d", className="gamebtn")
+surrender_button = html.Button("surrender (sur)", id="sur", className="gamebtn")
+doublestand_button = html.Button("double or stand (ds)", id="ds", className="gamebtn")
+hit_button = html.Button("hit (h)", id="h", className="gamebtn")
+stand_button = html.Button("stand (s)", id="s", className="gamebtn")
+das_button = html.Button("double after split (das)", id="das", className="gamebtn")
+split_button = html.Button("split (spl)", id="spl", className="gamebtn")
+
+
+layout: list = [
+    dbc.Row(
+        [
+            dbc.Col(user_input, width=2),
+            dbc.Col(gamemode_dd, width=2),
+            dbc.Col(correct_choice, width=2),
+            dbc.Col(start_button, width=2),
+        ],
+        id="button_row",
+    ),
+    html.Div([], id="bj-table"),
+    dbc.Row(
+        dbc.Col(
+            [surrender_button, doublestand_button, stand_button, hit_button, double_button, split_button, das_button],
+            id="button_row",
+        ),
+    ),
+]
