f5c915cf945f1d39e7889c0755e10d43868737a5
jonas.schrage <119843859+joschrag@users.noreply.github.com>
2024-06-17 18:19:01 +0200

    [#7]: Adjust deal_solo_cards to also deal 2 cards to the dealer

--- src/basic_strategy/mode_selector.py
+++ src/basic_strategy/mode_selector.py
@@ -7,20 +7,20 @@ SUITS = ["h", "c", "s", "d"]
 RANKS = ["2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"]
 
 
-def deal_cards(mode: str) -> list[Card]:
+def deal_solo_cards(mode: str) -> list[Card]:
     deck = [Card(s, r) for s, r in itertools.product(SUITS, RANKS)]
     if mode == "split":
-        cards = random.sample(deck, k=2)
-        return [cards[0], cards[0], cards[1]]
+        cards = random.sample(deck, k=3)
+        return [cards[0], *cards]
     if mode == "soft":
         ranks = RANKS[:-5]
         deck = [Card(s, r) for s, r in itertools.product(SUITS, ranks)]
-        cards = random.sample(deck, k=2)
-        return [Card("s", "A"), cards[0], cards[1]]
+        cards = random.sample(deck, k=3)
+        return [Card("s", "A"), *cards]
     if mode == "hard":
         ranks = RANKS[:-1]
         deck = [Card(s, r) for s, r in itertools.product(SUITS, ranks)]
-        cards = random.sample(deck, k=3)
+        cards = random.sample(deck, k=4)
         return cards
-    cards = random.sample(deck, k=3)
+    cards = random.sample(deck, k=4)
     return cards
