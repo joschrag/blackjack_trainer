19372b0a3f5453781d6d135b8621ad6005fbc880
jonas.schrage <119843859+joschrag@users.noreply.github.com>
2024-06-18 23:35:56 +0200

    [#7]: Add table dtypes

--- src/__init__.py
+++ src/__init__.py
@@ -2,9 +2,25 @@
 
 import pathlib
 
+import pandas as pd
 import sqlalchemy as sa
 
 db_file = pathlib.Path.cwd() / "db" / "db.sqlite"
 if not db_file.exists():
     db_file.touch()
 engine = sa.create_engine("sqlite:///db/db.sqlite")
+
+TABLE_DTYPES = {
+    "user": pd.StringDtype(),
+    "training_type": pd.StringDtype(),
+    "was_correct": pd.BooleanDtype(),
+    "correct_move": pd.StringDtype(),
+    "guessed_move": pd.StringDtype(),
+    "card1": pd.StringDtype(),
+    "card2": pd.StringDtype(),
+    "hand_value": pd.Int16Dtype(),
+    "dealer_card": pd.Int16Dtype(),
+    "upload_time": "datetime64[ns]",
+}
+
+EXT_TABLE_DTYPES = {**TABLE_DTYPES, "date": "datetime64[ns]", "count": pd.Int32Dtype(), "total": pd.Int32Dtype()}
