2477bae83ec46c42f137e4fd4ccca1e23e2a496d
jonas.schrage <119843859+joschrag@users.noreply.github.com>
2024-06-20 19:07:29 +0200

    [#9]: Move fixture into tests.fixtures script

--- tests/app/test_ui_callbacks.py
+++ tests/app/test_ui_callbacks.py
@@ -1,6 +1,4 @@
-import datetime
 from collections import Counter
-from typing import Generator
 
 import pandas as pd
 import pytest
@@ -8,31 +6,10 @@ import sqlalchemy as sa
 
 from src import EXT_TABLE_DTYPES
 from src.app import ui_callbacks as ui
+from tests.fixtures import setup_db  # noqa:F401
 
 
-@pytest.fixture(scope="session")
-def setup_db() -> Generator[tuple[sa.engine.Engine, pd.DataFrame], None, None]:
-    df = pd.DataFrame(
-        {
-            "user": pd.Series(["user1", "user2", "user1", "user2", "user1"], dtype=pd.StringDtype()),
-            "training_type": pd.Series(["basic", "basic", "soft", "hard", "split"], dtype=pd.StringDtype()),
-            "was_correct": pd.Series([True, True, False, False, True], dtype=pd.BooleanDtype()),
-            "correct_move": pd.Series(["d", "sur", "ds", "d", "spl"], dtype=pd.StringDtype()),
-            "guessed_move": pd.Series(["d", "sur", "d", "h", "spl"], dtype=pd.StringDtype()),
-            "card1": pd.Series(["3", "A", "A", "5", "2"], dtype=pd.StringDtype()),
-            "card2": pd.Series(["5", "5", "8", "6", "2"], dtype=pd.StringDtype()),
-            "hand_value": pd.Series([8, 16, 19, 11, 4], dtype=pd.Int16Dtype()),
-            "dealer_card": pd.Series([4, 9, 6, 11, 4], dtype=pd.Int16Dtype()),
-            "upload_time": pd.Series([datetime.datetime.now()] * 5, dtype="datetime64[ns]"),
-        }
-    )
-    engine = sa.create_engine("sqlite://")
-    with engine.begin() as conn:
-        df.to_sql("training_data", conn, if_exists="append")
-    yield engine, df
-
-
-def test_cb_load_data(setup_db: tuple[sa.engine.Engine, pd.DataFrame]) -> None:
+def test_cb_load_data(setup_db: tuple[sa.engine.Engine, pd.DataFrame]) -> None:  # noqa:F811
     engine, df = setup_db
     data = ui.load_data(1, engine)
     df["date"] = df["upload_time"].dt.date
@@ -65,7 +42,7 @@ def test_cb_activate_move_dropdown(status: bool) -> None:
 @pytest.mark.parametrize("move_switch,move_list", zip([True, False], [["h", "sur"], []]))
 @pytest.mark.parametrize("do_abs_vals", ["absolute values", "percent"])
 def test_cb_plot_data_callback(
-    setup_db: tuple[sa.engine.Engine, pd.DataFrame],
+    setup_db: tuple[sa.engine.Engine, pd.DataFrame],  # noqa:F811
     user_switch: bool,
     mode_switch: bool,
     move_switch: bool,
@@ -85,7 +62,7 @@ def test_cb_plot_data_callback(
     assert len(graphs) == 2 * max(1, len(user_list)) * max(1, len(mode_list))
 
 
-def test_cb_populate_user_dropdown(setup_db: tuple[sa.engine.Engine, pd.DataFrame]) -> None:
+def test_cb_populate_user_dropdown(setup_db: tuple[sa.engine.Engine, pd.DataFrame]) -> None:  # noqa:F811
     _, df = setup_db
     df["date"] = df["upload_time"].dt.date
     df["count"] = 1
@@ -95,7 +72,7 @@ def test_cb_populate_user_dropdown(setup_db: tuple[sa.engine.Engine, pd.DataFram
     assert Counter(users) == Counter(["user1", "user2"])
 
 
-def test_cb_populate_mode_dropdown(setup_db: tuple[sa.engine.Engine, pd.DataFrame]) -> None:
+def test_cb_populate_mode_dropdown(setup_db: tuple[sa.engine.Engine, pd.DataFrame]) -> None:  # noqa:F811
     _, df = setup_db
     df["date"] = df["upload_time"].dt.date
     df["count"] = 1
