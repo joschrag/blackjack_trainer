1714aa99ed19bbc068f924b1c50e4fb169ad7f2e
jonas.schrage <119843859+joschrag@users.noreply.github.com>
2025-06-23 23:01:44 +0200

    Change import for python3.10 and 3.11.

--- src/blackjack_trainer/setup_logging.py
+++ src/blackjack_trainer/setup_logging.py
@@ -11,8 +11,8 @@ import sys
 
 import yaml
 
-# Check if the Python version is 3.9
-if sys.version_info.major == 3 and sys.version_info.minor == 9:
+# Check if the Python version is older than 3.12
+if sys.version_info.major == 3 and sys.version_info.minor < 12:
     # pylint: disable-next=import-error
     from overrides import override
 else:
