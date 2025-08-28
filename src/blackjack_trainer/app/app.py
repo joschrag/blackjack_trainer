631757367113bb37c9018c531d0f8fb4cd6c9127
jonas.schrage <119843859+joschrag@users.noreply.github.com>
2025-08-28 19:51:53 +0200

    Add MantineProvider to wrap app layout.

--- src/blackjack_trainer/app/app.py
+++ src/blackjack_trainer/app/app.py
@@ -2,6 +2,7 @@
 
 import dash
 import dash_bootstrap_components as dbc
+import dash_mantine_components as dmc
 import diskcache
 from dash import DiskcacheManager, dcc, html
 
@@ -14,6 +15,7 @@ app = dash.Dash(
     use_pages=True,
     external_stylesheets=[dbc.themes.MATERIA, "assets/style.css"],
 )
+
 nav_link_style = {
     "margin": "1em 1em",
     "textAlign": "center",
@@ -53,20 +55,22 @@ navbar = dbc.Navbar(
     },
 )
 
-app.layout = dbc.Container(
-    children=[
-        navbar,
-        dbc.Row(html.Div(dash.page_container)),
-        dcc.Interval(id="1_min", interval=1000 * 60),
-        dcc.Interval(id="10_min", interval=1000 * 10 * 60),
-        dcc.Store("data_store", storage_type="session"),
-        dcc.Store("cards_store_train", storage_type="session"),
-        dcc.Store("cards_store_game", storage_type="session"),
-        dcc.Store("settings_store", storage_type="session"),
-        dcc.Store("game_store", storage_type="session"),
-        dcc.Store("chips_store", storage_type="session"),
-        dcc.Store("deck_store", storage_type="session"),
-    ],
-    className="dbc",
-    fluid=True,
+app.layout = dmc.MantineProvider(
+    dbc.Container(
+        children=[
+            navbar,
+            dbc.Row(html.Div(dash.page_container)),
+            dcc.Interval(id="1_min", interval=1000 * 60),
+            dcc.Interval(id="10_min", interval=1000 * 10 * 60),
+            dcc.Store("data_store", storage_type="session"),
+            dcc.Store("cards_store_train", storage_type="session"),
+            dcc.Store("cards_store_game", storage_type="session"),
+            dcc.Store("settings_store", storage_type="session"),
+            dcc.Store("game_store", storage_type="session"),
+            dcc.Store("chips_store", storage_type="session"),
+            dcc.Store("deck_store", storage_type="session"),
+        ],
+        className="dbc",
+        fluid=True,
+    )
 )
